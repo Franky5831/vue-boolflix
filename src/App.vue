ace33a808ff410e3abc425ab8fe93b7ea3c192b6
Franky5831 <fracianna5@gmail.com>
2022-05-10 19:48:10 +0200

    .

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <app-header />
+    <app-main />
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/header.vue'
+import AppMain from './components/main.vue'
+
+export default {
+  name: 'App',
+  components:{
+    AppHeader,
+    AppMain
+  }
+}
+
+
+</script>
+
+<style lang="scss">
+@import "src/styles/general.scss";
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+      background-color: #1E2D3B;
+}
+</style>
