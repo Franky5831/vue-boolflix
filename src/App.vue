ed6de578db60d77e304ec7edf37e1b37f01905a7
Franky5831 <fracianna5@gmail.com>
2022-05-10 19:52:46 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,6 @@ export default {
 </script>
 
 <style lang="scss">
-@import "src/styles/general.scss";
 *{
   margin: 0;
   padding: 0;
---
+++ src/components/header.vue
@@ -0,0 +1,26 @@
+<template>
+<header>
+    <h1>header</h1>
+</header>
+</template>
+
+
+<script>
+export default {
+    name: 'AppHeader',
+    methods:{}
+}
+
+
+</script>
+
+<style scoped lang="scss">
+header{
+    background-color: #2E3A46;
+
+    img{
+        height: 100px;
+        padding: 20px;
+    }
+}
+</style>
